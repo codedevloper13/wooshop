a91a25a8b0ff7a57792622413dd927ddc57c4d60
codedevloper13 <codedevloper13@gmail.com>
2023-01-30 11:27:05 +0530

    #4 GetStaticProps with Axios in Next.js | React WooCommerce REST API | Data Fetching | next js axios
    #4 GetStaticProps with Axios in Next.js | React WooCommerce REST API | Data Fetching | next js axios

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@
 import Head from "next/head";
 import Header from "../src/components/layouts/header/Header.js";
 import Footer from "../src/components/layouts/footer/Footer.js";
+import axios from "axios";
 
 export default function Home() {
 	return (
@@ -15,4 +16,16 @@ export default function Home() {
 			<Footer />
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const { data } = await axios.get(
+		`${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/wp-json/rae/v1/header-footer?header_location_id=hcms-menu-header&footer_location_id=hcms-menu-footer`
+	);
+	return {
+		props: {
+			data: data || {},
+		},
+		revalidate: 1,
+	};
 }
